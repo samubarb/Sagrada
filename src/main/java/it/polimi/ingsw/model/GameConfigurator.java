192bba3eb2d2f999c524ccc8be5680f814230cfc
Riccardo Cedroni <riccardomatteo.cedroni@mail.polimi.it>
2018-05-29 13:21:51 +0200

    update

--- src/main/java/it/polimi/ingsw/model/GameConfigurator.java
+++ src/main/java/it/polimi/ingsw/model/GameConfigurator.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw.model;
 
-public class GameConfigurator {
+import java.io.Serializable;
+
+public class GameConfigurator implements Serializable {
     public static final int DEFAUTL_VALUE = 0;
 
     private Game game;
@@ -21,6 +23,7 @@ public class GameConfigurator {
     public GameConfigurator(Game game) {
         this.game = game;
         game.configureGame();
+        createWindowPatternCards(game);
     }
 
     public void createWindowPatternCards(Game game){
