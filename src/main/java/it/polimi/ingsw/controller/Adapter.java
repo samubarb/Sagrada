7166c93bfd47a48c6525ddeb85ab659950259e37
WildStack <samuele.barbieri95@gmail.com>
2018-05-29 10:18:14 +0200

    add adapter colorToView(), to test

--- src/main/java/it/polimi/ingsw/controller/Adapter.java
+++ src/main/java/it/polimi/ingsw/controller/Adapter.java
@@ -1,12 +1,15 @@
 package it.polimi.ingsw.controller;
 
+import it.polimi.ingsw.model.Color;
 import it.polimi.ingsw.model.Dice;
 import it.polimi.ingsw.model.WindowPattern;
+import it.polimi.ingsw.view.VColor;
 import it.polimi.ingsw.view.VDice;
 import it.polimi.ingsw.view.VWindowPattern;
 
 public interface Adapter {
 
     VWindowPattern patternToView(WindowPattern pattern);
+    VColor ColorToView(Color color);
     //VDice DiceToView(Dice dice);
 }
