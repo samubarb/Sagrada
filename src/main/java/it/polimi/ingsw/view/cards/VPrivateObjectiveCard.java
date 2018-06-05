5a0db12399d0277713ef32f55ad07e0c0e13eb82
WildStack <samuele.barbieri95@gmail.com>
2018-06-05 16:03:37 +0200

    add objectiveCard toString()

--- src/main/java/it/polimi/ingsw/view/cards/VPrivateObjectiveCard.java
+++ src/main/java/it/polimi/ingsw/view/cards/VPrivateObjectiveCard.java
@@ -2,11 +2,18 @@ package it.polimi.ingsw.view.cards;
 
 import it.polimi.ingsw.view.other_elements.VColor;
 
+import static it.polimi.ingsw.inputoutput.IOManager.newline;
+
 public class VPrivateObjectiveCard extends VObjectiveCard{
     private VColor color;
 
     public VPrivateObjectiveCard(String name, VColor color) {
         super(name);
         this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return this.color + super.getName() + VColor.RESET + newline + super.getDescription();
     }
 }
