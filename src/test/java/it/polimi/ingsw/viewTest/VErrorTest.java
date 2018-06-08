08d66d80ff3205f2bb99460fa51f80a717dbe4cb
WildStack <samuele.barbieri95@gmail.com>
2018-06-08 18:43:38 +0200

    add VError and notifyError() tests, add visual fixes

---
+++ src/test/java/it/polimi/ingsw/viewTest/VErrorTest.java
@@ -0,0 +1,23 @@
+package it.polimi.ingsw.viewTest;
+
+import it.polimi.ingsw.view.CLI;
+import it.polimi.ingsw.view.View;
+import it.polimi.ingsw.view.other_elements.VError;
+import org.junit.jupiter.api.Test;
+
+import static it.polimi.ingsw.inputoutput.IOManager.println;
+
+public class VErrorTest {
+    @Test
+    public void VErrorPrintTest() {
+        for (VError e : VError.values())
+            println(e.toString());
+    }
+
+    @Test
+    public void VErrorNotify() {
+        View view = new CLI("TestPlayer");
+        for (VError e : VError.values())
+            view.notifyError(e);
+    }
+}
