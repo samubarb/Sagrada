c6c81766aafe9ac292901a8fd7f70519484bddc0
WildStack <samuele.barbieri95@gmail.com>
2018-07-06 18:59:01 +0200

    fix VGame gui test exceptions

--- src/test/java/it/polimi/ingsw/gui_test/VGameTest.java
+++ src/test/java/it/polimi/ingsw/gui_test/VGameTest.java
@@ -20,31 +20,20 @@ public class VGameTest extends Application {
         Adapter ada = new AdapterCLI();
         Game game = new Game();
 
-        game.configureGame();
-
         Player p1 = new Player("TestPlayer1", Color.RED);
         Player p2 = new Player("TestPlayer2", Color.BLUE);
         Player p3 = new Player("TestPlayer3", Color.GREEN);
         Player p4 = new Player("TestPlayer4", Color.PURPLE);
-
-        p1.setWindowPattern(new GameConfigurator(game).createWPCbatllo());
-        p2.setWindowPattern(new GameConfigurator(game).createWPCViaLux());
-        p3.setWindowPattern(new GameConfigurator(game).createWPCKaleidoscopicDream());
-        p4.setWindowPattern(new GameConfigurator(game).createWPCindustria());
-
-        p1.setFrame(new Frame());
-        p2.setFrame(new Frame());
-        p3.setFrame(new Frame());
-        p4.setFrame(new Frame());
 
         game.setAddPlayer(p1);
         game.setAddPlayer(p2);
         game.setAddPlayer(p3);
         game.setAddPlayer(p4);
 
-        GameConfigurator gconf = new GameConfigurator(game);
+        new GameConfigurator(game);
 
         VGame vgame = ada.gameToView(game);
+        vgame.setClientPlayer(p1.getName());
 
         Scene scene = new Scene(vgame.toGUI());
 
