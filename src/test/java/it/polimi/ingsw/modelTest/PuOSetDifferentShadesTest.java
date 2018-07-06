ed16fc1a3895703426ad2b0edf710b806acac43f
Riccardo Cedroni <riccardomatteo.cedroni@mail.polimi.it>
2018-07-06 15:48:17 +0200

    update model test

--- src/test/java/it/polimi/ingsw/modelTest/PuOSetDifferentShadesTest.java
+++ src/test/java/it/polimi/ingsw/modelTest/PuOSetDifferentShadesTest.java
@@ -5,16 +5,18 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class PuOSetDiffrentColorTest {
+public class PuOSetDifferentShadesTest {
 
     @Test
-    public void testCalculateScoreTest(){
+
+    public void testPuOSetDifferentShades(){
         Frame testFrame=new Frame();
-        Dice testDice1=new Dice(Color.RED,6);
-        Dice testDice2=new Dice(Color.GREEN,3);
-        Dice testDice3=new Dice(Color.BLUE ,1);
-        Dice testDice4=new Dice(Color.YELLOW,3);
-        Dice testDice5=new Dice(Color.PURPLE,3);
+        Dice testDice1=new Dice(Color.RED,1);
+        Dice testDice2=new Dice(Color.GREEN,2);
+        Dice testDice3=new Dice(Color.BLUE ,3);
+        Dice testDice4=new Dice(Color.YELLOW,4);
+        Dice testDice5=new Dice(Color.PURPLE,5);
+        Dice testDice6=new Dice(Color.PURPLE,6);
         testFrame.setPositionDice(testDice1,0,0);
         testFrame.setPositionDice(testDice2,0,1);
         testFrame.setPositionDice(testDice3,0,2);
@@ -23,15 +25,16 @@ public class PuOSetDiffrentColorTest {
         testFrame.setPositionDice(testDice3,1,0);
         testFrame.setPositionDice(testDice4,2,0);
         testFrame.setPositionDice(testDice5,3,0);
+        testFrame.setPositionDice(testDice6,3,1);
+
 
         WindowPattern testWindow=new WindowPattern(5,"prova");
         Player playerOne=new Player("gianfranco", Color.RED,testFrame,testWindow);
-        PuOSetDifferentColor testone=new PuOSetDifferentColor("prova",4);
+        PuOSetDifferentShades testone=new PuOSetDifferentShades("prova",5);
         System.out.println(testone.calculateScore(playerOne));
+        testone.calculateScore(playerOne);
 
-        assertTrue(testone.calculateScore(playerOne)==4);
-        //assertTrue(testLine2.calculateScore(playerOne)==5);
-
+        assertTrue(testone.calculateScore(playerOne)==5);
 
     }
 }
