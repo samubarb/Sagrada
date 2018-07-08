1487beb12662218ef39a311606adc99a4db2bd50
teochiari <matteo2.chiari@mail.polimi.it>
2018-07-08 18:06:44 +0200

    update controller and readme

---
+++ Deliverables/README.txt
@@ -0,0 +1,13 @@
+SAGRADA
+-Riccardo Matteo Cedroni
+-Matteo Chiari
+-Samuele Barbieri
+
+To start the project the file Hierarchy must be like this:
+|
+|-> Main folder of the project
+|-> JSONconf/
+
+The JSONConf can be find in the deliverables folder, it has to be copied as described above.
+
+The user has to first start the Server.jar that can be found in Deliverables and then start the Client.jar processes.
--- src/test/java/it/polimi/ingsw/controllerTest/ServerInterfaceImplTest.java
+++ src/test/java/it/polimi/ingsw/controllerTest/ServerInterfaceImplTest.java
@@ -6,6 +6,10 @@ import it.polimi.ingsw.controller.Server.ServerLauncher;
 import it.polimi.ingsw.controller.Server.User;
 import it.polimi.ingsw.controller.client.ClientLauncher;
 import it.polimi.ingsw.controller.client.rmiClient.RMIClientLauncher;
+import it.polimi.ingsw.model.Color;
+import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.GameConfigurator;
+import it.polimi.ingsw.model.Player;
 import org.junit.jupiter.api.Test;
 
 import java.rmi.RemoteException;
@@ -16,121 +20,19 @@ public class ServerInterfaceImplTest {
     /*
     This test checks getNumberOfPlayer
      */
-  /*  @Test
+    @Test
     public void testGetNicknames(){
         ServerLauncher serverLauncher = new ServerLauncher();
         ServerInterfaceImpl server = new ServerInterfaceImpl();
         server.setServerLauncher(serverLauncher);
         RMIClientLauncher clientLauncher = new RMIClientLauncher();
-        try {
-            server.register(clientLauncher, "dacco");
-            System.out.println(serverLauncher.getOfflineNicknames().size());
+            server.getServerLauncher().getNicknames().add(new User());
             System.out.println(serverLauncher.getNicknames().size());
-            System.out.println(serverLauncher.getNicknames().size()+serverLauncher.getOfflineNicknames().size());
+            System.out.println(serverLauncher.getNicknames().size());
             assertEquals(true, serverLauncher.getNicknames().size() == 1);
             assertEquals(true, serverLauncher.contains("dacco",serverLauncher.getNicknames()));
             assertEquals(false, serverLauncher.getNicknames().size() == 2);
             assertEquals(false, serverLauncher.contains("teo",serverLauncher.getNicknames()));
-        }
-        catch(RemoteException e){
-            System.out.println("error");
-        }
-    }*/
-    /*
-This test checks getOfflineNumberOfPlayer
- *//*
-    @Test
-    public void testGetOfflineNicknames(){
-        RMIClientLauncher serverLauncher = new RMIClientLauncher();
-        ServerInterfaceImpl server = new ServerInterfaceImpl();
-        server.setServerLauncher(serverLauncher);
-        try {
-            server.register(new RMIClientLauncher(), "dacco1");
-            server.getServerLauncher().getOfflineNicknames().add(new User("dacco", new RMIClientLauncher()));
-            assertEquals(true, server.getServerLauncher().getOfflineNicknames().size() == 1);
-            assertEquals(true, server.getServerLauncher().contains("dacco",serverLauncher.getOfflineNicknames()));
-            assertEquals(false, server.getServerLauncher().getOfflineNicknames().size() == 2);
-            assertEquals(false, server.getServerLauncher().contains("teo",serverLauncher.getOfflineNicknames()));
-        }
-        catch(Exception e){}
-    }*/
-    /**
-     * This test checks single and multi login, in particular checks the case in which there are two players logging with the same name
-     **//*
-    @Test
-    public void testRegisterSameName() {
-        System.out.println("testing");
-        ServerLauncher serverLauncher = new ServerLauncher();
-        ServerInterfaceImpl server = new ServerInterfaceImpl();
-        server.setServerLauncher(serverLauncher);
-        try {
-            assertEquals(true, server.register(new ClientLauncher(), "bob"));
-            assertEquals(false, server.register(new ClientLauncher(), "bob"));
-        } catch (RemoteException e) {
-        }
-    }*/
 
-    /**
-     * This test tests the case in whitch more than 4 player try to join the room
-     **/
-    /*   @Test
-    public void testRegister5DIfferentPlayer() {
-        System.out.println("testing");
-        ServerLauncher serverLauncher = new ServerLauncher();
-        ServerInterfaceImpl server = new ServerInterfaceImpl();
-        server.setServerLauncher(serverLauncher);
-        try {
-            assertEquals(true, server.register(new ClientLauncher(), "bob0"));
-            assertEquals(true, server.register(new ClientLauncher(), "bob1"));
-            assertEquals(true, server.register(new ClientLauncher(), "bob2"));
-            assertEquals(true, server.register(new ClientLauncher(), "bob3"));
-            assertEquals(false, server.register(new ClientLauncher(), "bob4"));
-        } catch (Exception e) {
-        }
     }
-*/
-    /**
-     * This test checks getNumberOfPlayer implementation
-     */
-  /*  @Test
-    public void testGetNumberOfPlayer() {
-        System.out.println("testing");
-        ServerLauncher serverLauncher = new ServerLauncher();
-        ServerInterfaceImpl server = new ServerInterfaceImpl();
-        server.setServerLauncher(serverLauncher);
-        try {
-            assertEquals(0, server.getNumberOfPlayer());
-            server.register(new ClientLauncher(), "bob0");
-            assertEquals(1, server.getNumberOfPlayer());
-            server.register(new ClientLauncher(), "bob1");
-            assertEquals(2, server.getNumberOfPlayer());
-            server.register(new ClientLauncher(), "bob2");
-            assertEquals(3, server.getNumberOfPlayer());
-            server.register(new ClientLauncher(), "bob3");
-            assertEquals(4, server.getNumberOfPlayer());
-            assertEquals(false, server.getNumberOfPlayer() == 5);
-        } catch (Exception e) {
-        }
-    }
-
-
-    @Test
-    public void testReconnectingInactivePlayer() {
-        ServerLauncher serverLauncher = new ServerLauncher();
-        ServerInterfaceImpl server = new ServerInterfaceImpl();
-        server.setServerLauncher(serverLauncher);
-        try {
-            server.register(new ClientLauncher(),"Dacco");
-            server.getServerLauncher().getOfflineNicknames().add(new User("teo", new ClientLauncher()));
-            //assertEquals(true, server.getServerLauncher().getOfflineNicknames().contains("teo"));
-            //assertEquals(false, server.getServerLauncher().getOfflineNicknames().contains("teo2"));
-            assertEquals(true, server.register(new ClientLauncher(), "teo"));
-            //assertEquals(true, server.getServerLauncher().getNicknames().contains("teo"));
-            assertEquals(true, server.getServerLauncher().getNicknames().size() == 2);
-            assertEquals(true, server.getServerLauncher().getOfflineNicknames().isEmpty());
-            assertEquals(false, server.getServerLauncher().getOfflineNicknames().contains("teo"));
-        }
-        catch(RemoteException e){}
-    }
-*/
 }
